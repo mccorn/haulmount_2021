c9ee3ac4e162dcec9acf1839f8b0476e83f3e08b
mccorn <mccorn13@gmail.com>
2021-03-08 13:55:35 +0300

    fix: clear

--- src/pages/MissionCard.js
+++ src/pages/MissionCard.js
@@ -13,11 +13,8 @@ export default class MissionCard extends Component {
   render() {
     const {data, filters} = this.props;
     const {selectSite, selectRocket} = filters;
-    // node?.links?.reddit?.launch
 
     if (!data) return null;
-    // if (selectSite && data?.links?.reddit?.launch !== selectSite) return null;
-    // if (selectRocket && data.rocket !== selectRocket) return null;
 
     const date = new Date(data.date_local);
 
